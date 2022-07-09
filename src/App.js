2a17ec32b08dc09cefed8b9a1965622ce67178ee
Muremwa <danmburu254@gmail.com>
2022-07-09 15:13:43 +0300

    adds create meeting page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Title from './pages/home/Title';
 import Error404 from './pages/home/Error404';
 import Home from './pages/home/Home';
+import CreateMeeting from './pages/meetingActions/CreateMeeting';
 
 
 function App () {
@@ -13,6 +14,7 @@ function App () {
                 <Title />
                 <Routes>
                     <Route path='/' element={<Home />} />
+                    <Route path='/new-meeting/' element={<CreateMeeting />}/>
                     <Route path='*' element={<Error404/>} />
                 </Routes>
             </BrowserRouter>
---
+++ src/pages/meetingActions/CreateMeeting.js
@@ -0,0 +1,71 @@
+// add 0 to numbers below ten: 9 -> 09, 12 -> 12, assumes all numbers are positive
+const zeroPen = (num) => num < 10? `0${num.toString()}`: num.toString();
+
+
+export default function CreateMeeting () {
+    // format a date for the backend
+    const fDate = (dateS) => {
+        const entDate = new Date(dateS);
+        const timezoneOffset = entDate.getTimezoneOffset() / -60;
+        const utcP = timezoneOffset < 0;
+        const offHours = parseFloat(timezoneOffset).toFixed(1).toString().split('.').map((t) => {
+            const t_ = parseInt(t);
+            return zeroPen(t_ < 0? -t_: t_);
+        });
+
+        const dt = {
+            year: entDate.getFullYear(),
+            month: zeroPen(entDate.getMonth()+1),
+            day: zeroPen(entDate.getDay()+1),
+            hour: zeroPen(entDate.getHours()),
+            minute: zeroPen(entDate.getMinutes()),
+            seconds: zeroPen(entDate.getSeconds()),
+            milliseconds: entDate.getMilliseconds(),
+            timezone: `${utcP? '-': '+'}${offHours.join(':')}`
+        };
+
+        return `${dt.year}-${dt.month}-${dt.day} ${dt.hour}:${dt.minute}:${dt.seconds}.${dt.milliseconds}${dt.timezone}`;
+    };
+
+    // when new meeting is schedules
+    const createMeetingHandler = (event_) => {
+        event_.preventDefault();
+        // get the data
+        const f = new FormData(event_.target);
+        const meetingData = {};
+        [...f.entries()].forEach(([key, entry]) => meetingData[key] = key.match(/time/g)? fDate(entry): entry);
+        [...event_.target].forEach((el) => el.disabled = true);
+
+        // TODO: add code to send details to backend, handle errors and success
+    };
+
+    return (
+        <div id='vnet-create-meeting'>
+            <h2>Schedule a new meeting</h2>
+
+            <form onSubmit={createMeetingHandler}>
+                <div>
+                    <label htmlFor="title-input">Meeting Title</label>
+                    <input type="text" name='title' id='title-input' placeholder='Enter title here' required={true}/>
+                </div>
+
+                <div>
+                    <label htmlFor="notes-input">Meeting notes</label>
+                    <textarea name="notes" id="notes-input" cols="30" rows="10" placeholder='Helpful notes about what the meeting is about' required={true}/>
+                </div>
+
+                <div>
+                    <label htmlFor="start_time-input">Start time</label>
+                    <input type="datetime-local" name='start_time' id='start_time-input' required={true}/>
+                </div>
+
+                <div>
+                    <label htmlFor="start_time-input">End time</label>
+                    <input type="datetime-local" name='end_time' id='end_time-input' required={true}/>
+                </div>
+
+                <input type="submit" value='Schedule meeting'/>
+            </form>
+        </div>
+    );
+}
