405e66f7f2819ef58a2debbafebce7144556b283
Muremwa <danmburu254@gmail.com>
2022-07-19 18:58:17 +0300

    simplify meeting component

--- src/pages/meeting/Meeting.js
+++ src/pages/meeting/Meeting.js
@@ -7,9 +7,19 @@ import SetUp from './SetUpMeeting';
 import { fetchMeeting, getUserDetails } from "../../utils/req";
 
 
+function MeetingLite (props) {
+    const [inMeeting, inMeetingSwitch] = useState(false);
+
+    return (
+        <div>
+            {inMeeting? <MeetingRoom switchMeeting={inMeetingSwitch}/>: <SetUp switchMeeting={inMeetingSwitch}/>}
+        </div>
+    )
+}
+
+
 export default function Meeting () {
     const { meetingCode } = useParams();
-    const [inMeeting, inMeetingSwitch] = useState(false);
     const { data: userData } = useQuery('user', getUserDetails);
     const { status, data: meetingData } = useQuery(['meeting', userData, meetingCode], () => fetchMeeting(meetingCode));
 
@@ -21,13 +31,12 @@ export default function Meeting () {
     }
 
     const meetingNotFound = (() => meetingData && meetingData.hasOwnProperty('message') && meetingData.message === 'Meeting not found')();
-    const mainView = inMeeting? <MeetingRoom switchMeeting={inMeetingSwitch}/>: <SetUp switchMeeting={inMeetingSwitch}/>;
 
     return (
         <div id="vnet-meeting">
             <h2>Welcome to VNET meeting</h2>
 
-            {meetingNotFound? <h3>Could not find the meeting with code "{meetingCode}"</h3>: mainView}
+            {meetingNotFound? <h3>Could not find the meeting with code "{meetingCode}"</h3>: <MeetingLite meeting={meetingData} user={userData}/>}
         </div>
     )
 }
