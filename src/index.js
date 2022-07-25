f7a094ea65b226b2af9ab1621588add28bb5f534
Muremwa <danmburu254@gmail.com>
2022-07-26 00:27:26 +0300

    removes stirct mode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <QueryClientProvider client={new QueryClient()}>
-            <App />
-        </QueryClientProvider>
-    </React.StrictMode>
+    <QueryClientProvider client={new QueryClient()}>
+        <App />
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
