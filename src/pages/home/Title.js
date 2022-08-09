b81aa55ee615fd2a96016f503f66684edac74e8b
Muremwa <danmburu254@gmail.com>
2022-08-09 16:10:40 +0300

    adds main and navigation

--- src/pages/home/Title.js
+++ src/pages/home/Title.js
@@ -1,9 +1,25 @@
+import reqData from '../../utils/wild';
+import '../style/Title.css';
+
+
 export default function Title (props) {
     const { user } = props;
+    const name = !user['firstName'] && !user['lastName']? user.username: `${user['firstName']} ${user['lastName']}`;
 
     return (
-        <div id="vnet-title">
-            <h1>This is VNET Calling Page ({user.username})</h1>
+        <div className="navbar" style={{backgroundImage: `url(${reqData.vnetBackgroundLarge})`}}>
+            <img src={reqData.vnetLogo} alt="Logo for VNET" className="navbar_vnet_logo" />
+
+                <div className="navbar_userDetails">
+                    <div className="navbar_userDetails_text">
+                        <p className="navbar_userDetails_username">{ name }</p>
+                        <span className="navbar_userDetails_email">{user['email']}</span>
+                    </div>
+
+                    <div className="navbar_userDetails_avatar">
+                        <img src={user['profileImage']} alt="user_avatar" className="navbar_avatar_img" />
+                    </div>
+                </div>
         </div>
     )
 }
