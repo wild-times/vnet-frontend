d4f1218328cfdd61e4c4a3b3d47ec6842fb250e9
Muremwa <danmburu254@gmail.com>
2022-08-10 20:06:34 +0300

    fixes error with send, peers now connecting

--- src/pages/meeting/peer/send.js
+++ src/pages/meeting/peer/send.js
@@ -1,14 +1,12 @@
 import { useRef, useState } from "react";
 
 export default function PeerShare (props) {
-    const [genCode, setGenCode] = useState(false);
     const { signalling, name, signalTypes, shareModal } = props;
+    const [randomCode, setRandomCode] = useState(0);
     const status = useRef(null);
     const statusPeer = useRef(null);
-    const randomCode = Math.floor(Math.random() * (1000000 - 100000 + 1) + 100000);
 
     const initSharing = () => {
-        setGenCode(true);
         const streamIds = [];
 
         // collect all streams
@@ -114,6 +112,12 @@ export default function PeerShare (props) {
         shareModal.current.style.display = '';
     };
 
+    const startSharing = () => setRandomCode(Math.floor(Math.random() * (1000000 - 100000 + 1) + 100000));
+
+    if (randomCode) {
+        initSharing();
+    }
+
     return (
         <div className="vnet-modal" id="host-peer" ref={shareModal}>
             <div className="vnet-modal-content peer-modal">
@@ -123,14 +127,16 @@ export default function PeerShare (props) {
                 </div>
                 <div className="vnet-modal-body">
                     {
-                        genCode? (
+                        randomCode? (
                             <>
                                 <p>Copy this code to the network peer to host a connection</p>
                                 <span>{ randomCode }</span>
                             </>
                         ): (
                             <div style={{width: '30%', margin: 'auto', marginTop: '2em', marginBottom: '2em'}}>
-                                <button onClick={initSharing} style={{background: 'azure', color: '#432943'}} className="wild-buttons">Start sharing</button>
+                                <button onClick={startSharing} style={{background: 'azure', color: '#432943'}} className="wild-buttons">
+                                    Start sharing
+                                </button>
                             </div>
                         )
                     }
